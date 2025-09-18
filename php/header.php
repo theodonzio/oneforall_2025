ada89280e9220bd40c734ef4dbad3ab822df2aa4
Valentino Grampin <valegrampin@gmail.com>
2025-09-18 11:55:24 -0300

    cambio de links

--- php/header.php
+++ php/header.php
@@ -6,15 +6,15 @@
   <ul class="nav nav-pills flex-column mb-auto">
     <p>General</p>
     <li class="nav-item">
-      <a href="#" class="nav-link active">
+      <a href="../php/index.php" class="nav-link active">
         <img src="../img/icons/home_icon.png" class="icono">
         Inicio
       </a>
     </li>
-    <li><a href="#" class="nav-link link-light"><img src="../img/icons/timetable_icon.png" class="icono"> Horarios</a></li>
-    <li><a href="#" class="nav-link link-light"><img src="../img/icons/space_icon.png" class="icono"> Espacios</a></li>
-    <li><a href="docentes.php" class="nav-link link-light"><img src="../img/icons/teach_icon.png" class="icono"> Docentes</a></li>
-    <li><a href="docentes.php" class="nav-link link-light"><img src="../img/icons/class_icon.png" class="icono"> Grupos</a></li>
+    <li><a href="../php/horarios.php" class="nav-link link-light"><img src="../img/icons/timetable_icon.png" class="icono"> Horarios</a></li>
+    <li><a href="../php/espacio.php" class="nav-link link-light"><img src="../img/icons/space_icon.png" class="icono"> Espacios</a></li>
+    <li><a href="../php/docentes.php" class="nav-link link-light"><img src="../img/icons/teach_icon.png" class="icono"> Docentes</a></li>
+    <li><a href="../php/grupos.php" class="nav-link link-light"><img src="../img/icons/class_icon.png" class="icono"> Grupos</a></li>
 
     <p class="mt-3">Herramientas</p>
     <li><a href="#" class="nav-link link-light"><img src="../img/icons/config_icon.png" class="icono">Configuración</a></li>
